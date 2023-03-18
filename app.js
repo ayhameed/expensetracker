2eb980564ac7aa5a0cc878899f4c91b9b9d35578
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2023-03-18 23:58:49 +0000

    used env variables for added security

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken')
 const bodyparser = require('body-parser');
 const users = require('./userModel');
 const _ = require('lodash');
@@ -26,40 +28,56 @@ require('./passportConfig')(passport);
 
 // login route
 app.get('/', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 
+//login post method
 app.post(
-  '/',
-  passport.authenticate('local-login', { session: false }),
-  (req, res, next) => {
-    res.redirect('/dashboard');
-    // res.json({
-    //   user: req.user,
-    // });
-  }
+    "/",
+    (req, res, next) => {
+        passport.authenticate('local-login', { session: false }, (err, user, info) => {
+            if (err || !user) {
+                return res.status(401).json({
+                    message: "Failed to login",
+                    token: null,
+                });
+            }
+            req.login(user, { session: false }, (err) => {
+                if (err) {
+                    return next(err);
+                }
+                // login successful, generate JWT
+                jwt.sign({ user: user }, process.env.SECRET_KEY, { expiresIn: '1h' }, (err, token) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: "Failed to login",
+                            token: null,
+                        });
+                    }
+                    // redirect to dashboard with JWT token
+                    return res.redirect('/dashboard?token=' + token);
+                });
+            });
+        })(req, res);
+    }
 );
+
 
 // signup route
 app.get('/signup', (req, res) => {
-  res.render('signup');
+    res.render('signup');
 });
 
 // signup post method
 app.post(
-  '/signup',
-  passport.authenticate('local-signup', { session: false }),
-  (req, res, next) => {
-    res.redirect('/dashboard');
-  }
+    '/signup',
+    passport.authenticate('local-signup', { session: false }),
+    (req, res, next) => {
+        res.redirect('/dashboard');
+    }
 );
 
-// dashboard route
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
-
 // app.listen
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server started on port 3000');
 });
